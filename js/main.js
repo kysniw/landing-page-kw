447ba8205c575c5b5e270a403f223550bff749f4
kysniw <kuba.winsyk.forgames@gmail.com>
2023-07-06 01:21:52 +0200

    A bit of code to repair mobile screens support. Improved scroll behavior.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,13 @@ headerBtn = document.querySelector(".header__link");
 contactSection = document.querySelector(".contact");
 footerBtn = document.querySelector(".footer__iconTop a");
 
-const showForm = () => {
+function showForm() {
   contactSection.classList.remove("hide");
-};
+  const self = this;
+  setTimeout(() => {
+    window.location.href = "#contact";
+  }, 300);
+}
 
 const hideForm = () => {
   contactSection.classList.add("hide");
